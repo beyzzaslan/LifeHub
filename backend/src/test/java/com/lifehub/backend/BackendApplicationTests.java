480dc53173958761e7833c4d4e51dd3c21458012
Beyza <beyzaaslan311@gmail.com>
2026-06-26 01:45:51 +0300

    initialize project structure

---
+++ backend/src/test/java/com/lifehub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifehub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
